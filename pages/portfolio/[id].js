c5851cd61bff36a788d5f581821841f27a8890cb
cloud-cottage <cooncn@gmail.com>
2026-02-26 16:16:27 +0800

    fix: 修复 portfolio/[id].js 导入路径

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,5 +1,5 @@
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
 
 export default function PortfolioDetailPage({ query }) {
   // 临时模拟数据，实际可从 CMS/API 获取
